83df09be7e3fdd9b863fe5184b075e04227ba265
Bach Tran <bach.tran@revature.com>
2021-09-09 11:24:47 -0500

    Add swapspace.sh file

--- java-with-automation/week-7/ec2-and-devops-related-materials/ec2-jenkins-setup.sh
+++ java-with-automation/week-7/ec2-and-devops-related-materials/ec2-jenkins-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # basics
 sudo yum install java-1.8.0-openjdk-devel -y
 
@@ -8,6 +10,10 @@ sudo yum install git -y
 wget https://mirrors.gigenet.com/apache/tomcat/tomcat-9/v9.0.52/bin/apache-tomcat-9.0.52.tar.gz
 tar -zxvf apache-tomcat-9.0.52.tar.gz
 
+# Important note is to change the port that Tomcat should run on from 8080 to something else
+# Because when you start up jenkins, it will listen on port 8080
+# You can't have two different processes listening to the same port
+
 # jenkins setup
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
@@ -15,3 +21,4 @@ sudo yum install -y jenkins
 
 # start jenkins
 sudo service jenkins start
+
