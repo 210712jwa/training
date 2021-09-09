426152e555ade92d6a00b3a3e39201c315c81fd8
Bach Tran <bach.tran@revature.com>
2021-09-09 13:43:09 -0500

    Update ec2-jenkins-setup.sh with additional information

--- java-with-automation/week-7/ec2-and-devops-related-materials/ec2-jenkins-setup.sh
+++ java-with-automation/week-7/ec2-and-devops-related-materials/ec2-jenkins-setup.sh
@@ -16,7 +16,13 @@ sudo yum install git -y
 wget https://mirrors.gigenet.com/apache/tomcat/tomcat-9/v9.0.52/bin/apache-tomcat-9.0.52.tar.gz
 tar -zxvf apache-tomcat-9.0.52.tar.gz
 
+# Startup tomcat by going to the tomcat folder -> bin, then run the startup.sh file
+# ./startup.sh
+# If you want to shutdown your Tomcat server, ./shutdown.sh
+
 # Important note is to change the port that Tomcat should run on from 8080 to something else
+# such as 8081
+# The way you do this is by editing the configuration inside of conf/server.xml
 # Because when you start up jenkins, it will listen on port 8080
 # You can't have two different processes listening to the same port
 
@@ -29,5 +35,10 @@ sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
 
 # start jenkins (MAKE SURE to change Tomcat to a different port first!)
-# sudo service jenkins start
+sudo service jenkins start
 
+# To have Jenkins have the ability to run sudo, you need to modify the /etc/sudoers file
+# sudo vim /etc/sudoers
+
+# Add this line to the file:
+# jenkins ALL=(ALL) NOPASSWD: ALL
